10260634704d6425756bde0fb8888afa8036db34
tiger42 <tiger42@gmxpro.de>
2020-03-28 18:47:59 +0100

    Made standard PHP session handling default

--- src/App/Context.php
+++ src/App/Context.php
@@ -6,6 +6,7 @@ use PtfDemo\Core\Auth\PtfDemo as Auth;
 use Ptf\Core\Session;
 use Ptf\Core\Session\File as FileSession;
 use Ptf\Core\Session\Memcache as MemcacheSession;
+use Ptf\Core\Session\Standard as StandardSession;
 use Ptf\View\Plain as PlainView;
 use Ptf\View\Smarty as SmartyView;
 
@@ -31,18 +32,26 @@ class Context extends \Ptf\App\Context
      */
     protected function init(): void
     {
+        //// Configure some individual routes just for demonstration purposes
         include 'routes.php';
         $this->routingTable = $routes;   // Set the included override routes
 
-        // Initialize the view to use
-//        $this->view = new SmartyView($this->getConfig('ViewSmarty'), $this);
-//         $this->view = new PlainView($this->getConfig('ViewPlain'), $this);   // You may omit this line, the Plain view is used as a fallback anyway
+        //// Initialize the view to use (choose one)
+        /* Smarty */
+        // $this->view = new SmartyView($this->getConfig('ViewSmarty'), $this);
+        /* Plain PHP */
+        $this->view = new PlainView($this->getConfig('ViewPlain'), $this);   // You may omit this line, the Plain view is used as a fallback anyway
 
-        // Initialize the session to use
-//        $this->session = MemcacheSession::getInstance();
-//        $this->session->init($this->getConfig('SessionMemcache'), $this);
-         $this->session = FileSession::getInstance();
-         $this->session->init($this->getConfig('SessionFile'), $this);
+        //// Initialize the session to use (choose one)
+        /* Memcached */
+        // $this->session = MemcacheSession::getInstance();
+        // $this->session->init($this->getConfig('SessionMemcache'), $this);
+        /* File system storage */
+        // $this->session = FileSession::getInstance();
+        // $this->session->init($this->getConfig('SessionFile'), $this);
+        /* Standard PHP */
+        $this->session = StandardSession::getInstance();
+        $this->session->init($this->getConfig('Session'), $this);
 
         $this->session->start();
     }
